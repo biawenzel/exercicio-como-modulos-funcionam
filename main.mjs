6c75c31a07644f564de4297d00f0671f9702c0b8
biawenzel <103464198+biawenzel@users.noreply.github.com>
2022-05-08 18:06:16 -0300

    Add files via upload

---
+++ main.mjs
@@ -0,0 +1,6 @@
+import { mostraIdade, mostraCidade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade("Beatriz", 29));
+console.log(mostraCidade("Beatriz", "Bauru"));
+console.log(mostraHobby("Beatriz", "ver seriados"));
+
